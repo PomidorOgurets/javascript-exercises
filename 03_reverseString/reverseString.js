3cc1a10dfe2539ac81aebd5926568ec4af5c3ea4
Your Name <163199516+PomidorOgurets@users.noreply.github.com>
2024-11-10 14:38:10 +0300

    feat: task 03_reverseString complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let finalString = ''
+    const string_array = [...string];
+    for (let i = string_array.length - 1; i >= 0; i--) {
+        finalString += string_array[i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
