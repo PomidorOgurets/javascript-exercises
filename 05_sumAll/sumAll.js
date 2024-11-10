41917a20c44f8b3e8ce5d27e3a03f7c9ace74fec
Your Name <163199516+PomidorOgurets@users.noreply.github.com>
2024-11-10 15:25:32 +0300

    feat: task 05_sumAll complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    if ((firstInt < 0) || (secondInt < 0)) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(secondInt - firstInt)) {
+        return 'ERROR';
+    }
+    if (typeof(firstInt) != 'number' || typeof(secondInt) != 'number') {
+        return 'ERROR';
+    }
+    secondInt > firstInt ? bigInt = secondInt : bigInt = firstInt;
+    secondInt > firstInt ? smallInt = firstInt : smallInt = secondInt;
+    return (bigInt + smallInt) / 2 * (bigInt - smallInt + 1);
 };
 
 // Do not edit below this line
